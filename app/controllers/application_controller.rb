5f36ae240147221cd211bbe8e09b5e5d7e737e2d
gdicera <giuseppedicera@uchicago.edu>
2023-01-26 15:30:44 +0000

    started vailocal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def software
+    render({ :template => "form_templates/software.html.erb"})
+  end
+  def company
+    render({ :template => "form_templates/company.html.erb"})
+  end
+  def applicant
+    render({ :template => "form_templates/applicant.html.erb"})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-
-
+  get("/software", { :controller => "application", :action => "software" })
+  get("/software/company", { :controller => "application", :action => "company" })
+  get("/software/applicant", { :controller => "application", :action => "applicant" })
 end
